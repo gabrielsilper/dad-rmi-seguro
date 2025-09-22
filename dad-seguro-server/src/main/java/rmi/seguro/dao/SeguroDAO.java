9c736b13164dcb87d8beb2ad21b1a1526009d7c5
gabrielsilper <gabrielsilper.main@gmail.com>
2025-09-22 13:56:14 -0400

    chore: altera consultas no SeguroDAO para usar SQL nativo

--- dad-seguro-server/src/main/java/rmi/seguro/dao/SeguroDAO.java
+++ dad-seguro-server/src/main/java/rmi/seguro/dao/SeguroDAO.java
@@ -10,8 +10,10 @@ import java.util.List;
 public class SeguroDAO {
     public List<SeguroEntity> listarTodos() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<SeguroEntity> query = session.createQuery("FROM SeguroEntity", SeguroEntity.class);
-            return query.getResultList();
+            return session.createNativeQuery(
+                    "SELECT * FROM seguros",
+                    SeguroEntity.class
+            ).getResultList();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -20,8 +22,8 @@ public class SeguroDAO {
 
     public List<SeguroEntity> buscarPorDestino(String destino) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<SeguroEntity> query = session.createQuery(
-                    "FROM SeguroEntity WHERE destino LIKE :destino",
+            Query<SeguroEntity> query = session.createNativeQuery(
+                    "SELECT * FROM seguros WHERE destino LIKE :destino",
                     SeguroEntity.class);
             query.setParameter("destino", "%" + destino + "%");
             return query.getResultList();
